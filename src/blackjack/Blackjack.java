752e4e5d9f23a8bac863516fbbce455caa89382a
Arshpartap <sin11503@sheridancollege.ca>
2022-04-15 17:04:54 -0400

    Blackjack version 2.0

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -8,14 +8,17 @@ import java.util.Scanner;
  */
 public class Blackjack {
     
-    public static boolean wonGame;
-    public static boolean lostGame;
-    public static boolean pushGame;
+//    public static boolean wonGame;
+//    public static boolean lostGame;
+//    public static boolean pushGame;
+//    public static int score;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int score;
+        score = 0;
         System.out.println("Welcome to Blackjack");
         System.out.println("Please enter your username: ");
         Scanner input = new Scanner(System.in);
@@ -24,299 +27,166 @@ public class Blackjack {
         boolean repeat = true;
         boolean repeat1 = true;
         
+//        boolean wonGame = false;
+//        boolean lostGame = false;
+//        boolean pushGame = false;
         
-            Hand playerHand = new Hand();
-            
-            Hand dealerHand = new Hand();
-            
-            dealerHand.addCard();
-        
-        while(repeat){
-            
-            while(repeat1){
             
             
             
             
-//            playerHand.addCard();
-//            playerHand.addCard();
-//            
-//            dealerHand.addCard();
+            while (repeat){
+                
+                
+                boolean wonGame = false;
+                boolean lostGame = false;
+                boolean pushGame = false;
+                
+                
+                Hand playerHand = new Hand();
             
+                Hand dealerHand = new Hand();
             
-            System.out.println("your card 1 is "+playerHand.addCard());
-            System.out.println("your card 2 is "+playerHand.addCard());
-            System.out.println("your hand value is "+playerHand.handValue);
-            
-            
-            System.out.println("\nDealer's face up card is "+dealerHand.addCard());
-            System.out.println("dealer's total is "+dealerHand.handValue);
-            
-            
-            
-            
-            if(playerHand.handValue == 21){
-                wonGame = true;
-                System.out.println("BLACKJACK!!!!");
+                dealerHand.addCard();
+                
+                
+                
+                System.out.println("\nYour card 1 is "+playerHand.addCard());
+                System.out.println("Your card 2 is "+playerHand.addCard());
+                System.out.println("Your hand value is "+playerHand.handValue);
+                
+                
+                
+                System.out.println("\nDealer's face up card is "+dealerHand.addCard());
+                System.out.println("dealer's total value is "+dealerHand.handValue);
+                
+                
+                
+                
+                if (playerHand.handValue == 21){
+                    System.out.println("\nBLACKJACK!!!! YOU WON!!");
+                    score++;
+                    wonGame = true;
+                    //break;
+                }
+                
+                if (playerHand.handValue > 21){
+                    System.out.println("\nYou lost. Your hand value went more than 21");
+                    lostGame = true;
+                    //break;
+                }
+                
+                if (dealerHand.handValue > 21){
+                    System.out.println("\nDealer's hand value went over 21. YOU WON!!!!");
+                    score++;
+                    wonGame = true;
+                    //break;
+                }
+                
+                
+               
+                
+                
+                //if(playerHand.handValue < 21 && dealerHand.handValue < 21){
+                
+                
+                if(!wonGame && !lostGame && !pushGame){
+                while (playerHand.handValue <= 21){
+                    
+                    System.out.println("\nWould you like to (1)Hit or (2)Stand ?");
+                    int hos = input.nextInt();
+                    if (hos == 1){
+                        System.out.println("\nYou drew "+playerHand.addCard());
+                        System.out.println("You hand value is "+playerHand.handValue);
+                        
+                        if(playerHand.handValue > 21){
+                            System.out.println("\nYour hand value went over 21. You lost");
+                            lostGame = true;
+                            break;
+                        }
+                        
+                        
+                    }
+                    else { break; }
+                    
+                    
+                    
+                }
+                
             }
-            
-            
-            
-            
-            if(playerHand.handValue > 21){
-                lostGame = true;
-                System.out.println("You bust! Your total went more than 21");
-                break;
-            }
-            if(dealerHand.handValue > 21){
-                wonGame = true;
-                System.out.println("Dealer busts! Dealer's total went more than 21. YOU WON!!");
-                break;
-            }
-            
-            
-            
-            if(playerHand.handValue < 21){
-                int playerCount = 3;
-                int dealerCount = 3;
                 
-                while(playerHand.handValue < 21){
-            System.out.println("\nDo you want to (1)Hit or (2)Stand ?");
-            int cont;
-            cont = input.nextInt();
-            
-            if(cont == 1){
-                System.out.println("your card "+playerCount+" is "+playerHand.addCard());
-                System.out.println("your hand value is now "+playerHand.handValue);
-                if(playerHand.handValue > 21){
-                lostGame = true;
-                System.out.println("You bust! Your total went more than 21");
-                //break;
-                }
                 
-                if(wonGame){
-                    break;
-                }
-                else if(lostGame){
-                    break;
-                }
-                else if(pushGame){
-                    break;
-                }
                 
-//                else if(playerHand.handValue == 21){
-//                    break;
-//                }
-                playerCount++;
                 
-//                if(wonGame){
-//                    break;
-//                }
-//                else if(lostGame){
-//                    break;
-//                }
-//                else if(pushGame){
-//                    break;
-//                }
+                //if (playerHand.handValue <= 21 && dealerHand.handValue <= 17){
                 
-                //break;
-            }
-                //break;
-                if(wonGame){
-                    break;
-                }
-                else if(lostGame){
-                    break;
-                }
-                else if(pushGame){
-                    break;
-                }
                 
-                //break;
+                if(!wonGame && !lostGame && !pushGame){
+                while(dealerHand.handValue <= 17){
+                    System.out.println("\nDealer drew "+dealerHand.addCard());
+                    System.out.println("dealer's hand value is "+dealerHand.handValue);
+                }
                 }
                 
                 
-                if(wonGame){
-                    break;
-                }
-                else if(lostGame){
-                    break;
-                }
-                else if(pushGame){
-                    break;
-                }
-            
-            
-            while(dealerHand.handValue <= 17){
-                System.out.println("dealer's card "+dealerCount+" is "+dealerHand.addCard());
-                System.out.println("dealer's hand value is now "+dealerHand.handValue);
-                dealerCount++;
-            }
-            
-            
-            if(dealerHand.handValue > 21){
-                lostGame = true;
-                System.out.println("Dealer busts! Dealer's total went more than 21. YOU WON!!");
-                break;
-            }
-            
-            
-            
-            if(playerHand.handValue > dealerHand.handValue){
-                wonGame = true;
-                System.out.println("YOU WON!!");
-                break;
-            }
-            else if(playerHand.handValue < dealerHand.handValue){
-                lostGame = true;
-                System.out.println("You lose. Dealer's hand value is more than you");
-                break;
-            }
-            else {
-                pushGame = true;
-                System.out.println("It's a push. No one wins");
-                break;
-            }
-            }  // WHILE LOOP}
-            
-                
-                if(wonGame){
-                    break;
-                }
-                else if(lostGame){
-                    break;
-                }
-                else if(pushGame){
-                    break;
-                }
                 
                 
-             int dealerCount = 3;   
-            
-            while(dealerHand.handValue <= 17){
-                System.out.println("dealer's card "+dealerCount+" is "+dealerHand.addCard());
-                System.out.println("dealer's hand value is now "+dealerHand.handValue);
+                if(!wonGame && !lostGame && !pushGame){
                 if(dealerHand.handValue > 21){
-                wonGame = true;
-                System.out.println("Dealer busts! Dealer's total went more than 21. YOU WON!!");
-                break;
-            }
-                
-                
-                
-                
-                if(wonGame){
-                    break;
+                    System.out.println("\nDealer's hand value went over 21. YOU WON!!!!");
+                    score++;
+                    wonGame = true;
+                    //break;  eh vala vi
                 }
-                else if(lostGame){
-                    break;
+                else{
+                    
+                    if(dealerHand.handValue > playerHand.handValue){
+                        System.out.println("\nYou lost. Dealer's hand value is more than your's");
+                        lostGame = true;
+                    }
+                    
+                    if(dealerHand.handValue < playerHand.handValue){
+                        System.out.println("\nYour hand value is more than the dealer's. YOU WON!!!!");
+                        score++;
+                        wonGame = true;
+                    }
+                    
+                    if(dealerHand.handValue == playerHand.handValue){
+                        System.out.println("\nIt's a push. Your hand is value is equal to the dealer's");
+                        pushGame = true;
+                    }
+                    
+                    
                 }
-                else if(pushGame){
-                    break;
-                }
+            }
+                
+                
+                //eh vala
                 
                 
                 
+                System.out.println("\nYour score is "+score);
                 
-                if(playerHand.handValue > dealerHand.handValue){
-                wonGame = true;
-                System.out.println("YOU WON!!");
-            }
-            else if(playerHand.handValue == dealerHand.handValue){
-                pushGame = true;
-                System.out.println("It's a push. No one wins");
-            }
-            else {
-                lostGame = true;
-                System.out.println("You lose. Dealer's hand value is more than you");}
-            
-            }
-            }
                 
-            
-            
-            if(wonGame){
-                    break;
+                System.out.println("\nWould you like to play another round? (If yes, enter 1)");
+                int choice = input.nextInt();
+                if(choice == 1){
+                    repeat = true;
                 }
-                else if(lostGame){
-                    break;
-                }
-                else if(pushGame){
-                    break;
-                }
-            
-            
-            
-            
-            if(playerHand.handValue > 21){
-                lostGame = true;
-                System.out.println("You bust! Your total went more than 21");
-                break;
-            }
-            if(dealerHand.handValue > 21){
-                wonGame = true;
-                System.out.println("Dealer busts! Dealer's total went more than 21. YOU WON!!");
-                break;
-            }
-            
-            if(playerHand.handValue > dealerHand.handValue){
-                wonGame = true;
-                System.out.println("YOU WON!!");
-            }
-            else if(playerHand.handValue == dealerHand.handValue){
-                pushGame = true;
-                System.out.println("It's a push. No one wins");
-            }
-            else {
-                lostGame = true;
-                System.out.println("You lose. Dealer's hand value is more than you");
+                else{ repeat = false; }
+                
+                
             }
             
             
-            
-            
-            
-            
-            
-            
-            
-//            if(wonGame){
-//                System.out.println("You won!!");
-//            }
-//            else if(lostGame){
-//                System.out.println("You lost");
-//            }
-//            else if(pushGame){
-//                System.out.println("It's a push");
-//            }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            System.out.println("Would you like to play another round? (If yes, enter 1)");
-            int choice = input.nextInt();
-            if(choice != 1){
-                repeat1 = false;
-            }
-        }
-            
-             System.out.println("Would you like to play another round? (If yes, enter 1)");
-            int choice = input.nextInt();
-            if(choice != 1){
-                repeat = false;
-            }
-            
-            
-        }
+        
+        
         
        
             
     }
+    
+}
 
             
 
--- src/blackjack/Suit1.java
+++ src/blackjack/Suit1.java
@@ -13,18 +13,18 @@ public class Suit1 {
     public String suitName;
     
     
-    public Suit1(){
-       
-    }
+//    public Suit1(){
+//       
+//    }
     
     public Suit1(int index){
         suit = suits[index];
     }
     
-    public String suitName(int index){
-        suitName = this.suits[index];
-        return suitName;
-    }
+//    public String suitName(int index){
+//        suitName = this.suits[index];
+//        return suitName;
+//    }
     
     @Override
     public String toString() {
